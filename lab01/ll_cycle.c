e0c5e8a06880f7fb0347d8294b5430954370a26e
z01ly <lingyzh@outlook.com>
2023-01-29 19:03:33 +0100

    lab0 and lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,38 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdbool.h>
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+       	if (head == NULL) {
+		return 0;
+	}
+
+       	node *tortoise = head;
+    	node *hare = head;
+       
+	while (true) {
+		hare = hare->next;
+		if (hare == NULL) {
+		       	break;
+		}
+		if(hare == tortoise) {
+		       	return 1;
+		}
+		
+		hare = hare->next;
+		if (hare == NULL) {
+			break;
+		}
+		if (hare == tortoise) {
+			return 1;
+		}
+		
+		tortoise = tortoise->next;
+		if (hare == tortoise) {
+			return 1;
+		}
+	}
+	
+	return 0;
 }
