bf3cdd8519d50a3d19ad4e45ca8f204364d2b721
z01ly <lingyzh@outlook.com>
2023-03-05 20:18:42 +0100

    lab3

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,19 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    beq a0, x0, special
+    addi sp, sp, -4
+    sw s0, 0(sp)
+    addi s0, a0, -1
+loop:
+    beq x0, s0, exit
+    mul a0, a0, s0
+    addi s0, s0, -1
+    j loop
+exit:
+    lw s0, 0(sp)
+    addi sp, sp, 4
+    jr ra # ret
+special:
+    addi a0, x0, 1
+    jr ra
